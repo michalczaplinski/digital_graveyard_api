2ad79d7c5cab129f6bcf2f509140c647ba6e5a72
Michal Czaplinski <mmczaplinski@gmail.com>
2016-07-13 17:09:55 +0200

    first try of the API

--- api.py
+++ api.py
@@ -1,12 +1,21 @@
-import os
-from flask import Flask
+import os, os.path, sqlite3
+from flask import Flask, g, jsonify
 
 app = Flask(__name__)
 app.config.from_pyfile('flaskapp.cfg')
+DATABASE = os.path.join(os.getenv('OPENSHIFT_DATA_DIR'), 'tweets.db')
+INCLUDE_RETWEETS = False
 
-@app.route('/hello')
-def index():
-    return 'hello'
+@app.before_request
+def before_request():
+    g.db = sqlite3.connect(DATABASE)
+
+
+@app.route("/tweets")
+def hello():
+    tweets = g.db.execute("SELECT text, user, time, name, retweet_status FROM tweet").fetchall()
+    return jsonify(tweets)
+
 
 # @app.route('/<path:resource>')
 # def serveStaticResource(resource):
@@ -17,4 +26,4 @@ def index():
 #     return "<strong>It's Alive!</strong>"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
