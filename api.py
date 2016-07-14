bff68fd23f0632c849a74c1583a351c125c7cf4e
Michal Czaplinski <mmczaplinski@gmail.com>
2016-07-14 02:41:05 +0200

    fucking cors again

--- api.py
+++ api.py
@@ -1,9 +1,10 @@
 import os, os.path, sqlite3, json
 from flask import Flask, g, jsonify
-from flask_cors import CORS, cross_origin
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
-CORS(app)
+api = Api(app)
+
 app.config.from_pyfile('flaskapp.cfg')
 DATABASE = os.path.join(os.getenv('OPENSHIFT_DATA_DIR'), 'tweets.db')
 INCLUDE_RETWEETS = False
@@ -12,18 +13,19 @@ INCLUDE_RETWEETS = False
 def before_request():
     g.db = sqlite3.connect(DATABASE)
 
+class Db(Resource):
+    def get(self):
+        tweets = g.db.execute("SELECT text, user, time, name, retweet_status FROM tweet").fetchall()
+        return jsonify(tweets)
 
-@app.route("/db")
-def tweets():
-    tweets = g.db.execute("SELECT text, user, time, name, retweet_status FROM tweet").fetchall()
-    return jsonify(tweets)
+class File(Resource):
+    def get(self):
+        tweets_file = open(os.path.join(os.getenv('OPENSHIFT_DATA_DIR'), 'tweets.json'), 'r')
+        tweets = json.load(tweets_file)
+        return jsonify(tweets)
 
-@app.route("/file")
-def tweets_from_file():
-    tweets_file = open(os.path.join(os.getenv('OPENSHIFT_DATA_DIR'), 'tweets.json'), 'r')
-    tweets = json.load(tweets_file)
-    return jsonify(tweets)
-
+api.add_resource(Db, '/db')
+api.add_resource(File, '/file')
 
 if __name__ == '__main__':
     app.run(debug=True)
